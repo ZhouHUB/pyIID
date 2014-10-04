50c5b36ea06895b7d9c7beb55b693dab9f287935
Christopher J. Wright <cjwright4242@gmail.com>
2014-10-04 19:38:59 -0400

    Wrote a bunch of kernels for the gradient method.

--- pyiid/gpu/kernels.py
+++ pyiid/gpu/kernels.py
@@ -13,7 +13,11 @@ def Gr_grad(grad, q, exp_data, delta_qi):
             #race condition problem here
             new_q = q
             new_q[i] += delta_qi
-            #I am not certain where everything should be going here
+            #I am not certain where everything should be going here in terms
+            # of keeping track of the variable memory
+            #Also get_Gr_U in this place is rather wasteful, need to move to
+            # more efficient method, only recalcuating parts of the distance
+            # matrix that changed
             get_Gr_U(U, new_q, exp_data)
             #This won't work, this would require me to wait for each get_Gr_U
             #  to finish before starting the next batch
@@ -96,16 +100,64 @@ def get_FQ_matrix(FQ, reduced_D, scatter_array, Q):
                                                                           ty]
 
 '''__device__ void generate_D(double* D, double* q){
-    int tx = threadIdx.x
-    int ty = threadIdx.y
-    int tz = threadIdx.z
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+    int tz = threadIdx.z;
 
-    D[tx, ty, tz] = q[ty, tz] - q[tx, tz]
+    D[tx, ty, tz] = q[ty, tz] - q[tx, tz];
 }'''
 
 #generate reduced D
 '''__device__ void reduce_D(double* reduced_D, double* D){
-    int tx = threadIdx.x
-    int ty = trheadIdx.y
-    reduced_D[tx, ty] = sqrt(D[tx, ty, 0]**2+D[tx, ty, 1]**2+D[tx, ty, 2]**2)
+    int tx = threadIdx.x;
+    int ty = trheadIdx.y;
+    reduced_D[tx, ty] = sqrt(D[tx, ty, 0]**2+D[tx, ty, 1]**2+D[tx, ty, 2]**2);
 }'''
+
+'''__global__ get_rw(double RW, double* Gr, double* exp_data){
+    rw_top<>(double* top, double* Gr, double* exp_data);
+    rw_bottom<>(double* bot, double* Gr);
+    one_to_zero_sum<>(double red_top, double* top);
+    one_to_zero_sum<>(double red_bot, double* bot);
+    RW = (red_top/red_bot)**2;
+}'''
+'''
+__device__ rw_top(double top, double* Gr, double* exp_data){
+    int tx = threadIdx.x;
+    top[tx] = (exp_data[tx]- Gr[tx])**2;
+}'''
+'''
+__device__ rw_bottom(double bot, double* Gr, double* exp_data){
+    int tx = threadIdx.x
+    bot[tx] = (exp_data[tx]- Gr[tx])**2
+}'''
+'''
+__device__ get_FQ_matrix(double* FQ, double* reduced_D, double*
+scatter_array, double* Q){
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+    int tz = threadIdx.z;
+    if (tx == ty){
+        FQ3[tx, ty, tz] = 0
+        }
+    else{
+        #technicly scatter_array[ty] is the complex conjugate
+        FQ3[tx, ty, tz] = scatter_array[tx]*scatter_array[ty]*Sin(Q[
+                                                                      tz]*reduced_D[tx,
+                                                                ty])/reduced_D[tx,
+                                                                          ty]
+        }
+}'''
+'''
+__global__ get_F(double* F, double* reduced_D, double* scatter_matrix,
+double ave_scatter, double* Q){
+    get_FQ_matrix<>(FQ3, reduced_D, scatter_matrix, Q)
+    _syncthreads()
+    three_to_two_sum<>(FQ2, FQ3)
+    _syncthreads()
+    two_to_one_sum<>(F, FQ2)
+    _syncthreads()
+    prefactor = 1/len(reduced_D)/ave_scatter
+    F *= prefactor
+    //NEED TO PAD F WITH ZEROS FROM 0 TO QMIN
+}
