b2bc5c203524a0f400c3f875cb6eb3d04cb4f367
Christopher J. Wright <cjwright4242@gmail.com>
2014-11-21 13:08:51 -0500

    MTN: PEP8

--- pyiid/serial_kernel.py
+++ pyiid/serial_kernel.py
@@ -33,17 +33,20 @@ def get_fq_array(fq, r, scatter_array, n_range, Qmax_Qmin_bin_range, Qbin):
                 for kq in Qmax_Qmin_bin_range:
                     dwscale = 1
                     fq[kq] += smscale * dwscale * scatter_array[tx,
-                                                            kq] * scatter_array[
-                              ty, kq] / r[tx, ty] * math.sin(
-                    kq * Qbin * r[tx, ty])
+                                                                kq] * \
+                              scatter_array[
+                                  ty, kq] / r[tx, ty] * math.sin(
+                        kq * Qbin * r[tx, ty])
 
 
-def get_normalization_array(norm_array, scatter_array, Qmax_Qmin_bin_range, n_range):
-        for kq in Qmax_Qmin_bin_range:
-            for tx in n_range:
-                for ty in n_range:
-                    norm_array[kq] += (scatter_array[tx, kq] * scatter_array[ty, kq])
+def get_normalization_array(norm_array, scatter_array, Qmax_Qmin_bin_range,
+                            n_range):
+    for kq in Qmax_Qmin_bin_range:
+        for tx in n_range:
+            for ty in n_range:
+                norm_array[kq] += (
+                    scatter_array[tx, kq] * scatter_array[ty, kq])
 
-        norm_array*1/(scatter_array.shape[0])**2
+                # norm_array*1/(scatter_array.shape[0])**2
 
-# def get_pdf_at_Qmin(qmin):
+                # def get_pdf_at_Qmin(qmin):
