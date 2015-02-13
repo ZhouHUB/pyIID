FIX: Fixed bug in grad_rw, which lead to the wrong gradient being generated

pyiid/kernels/serial_kernel.py
@@ -343,9 +343,9 @@ def get_grad_rw(grad_rw, grad_pdf, gcalc, gobs, rw, scale, weight=None):
     n = len(grad_pdf)
     for tx in range(n):
         for tz in range(3):
-            part1 = 1.0 / np.sum(weight[:] * (scale * gcalc[:] - gobs[:]))
-            part2 = np.sum(scale * grad_pdf[tx, tz, :])
-            grad_rw[tx, tz] = rw.real / part1.real * part2.real
+            part1 = np.sum((gobs[:] - gcalc[:])**2)
+            part2 = np.sum((gcalc[:] - gobs[:]) * grad_pdf[tx, tz, :])
+            grad_rw[tx, tz] = rw.real * part2.real / part1.real
 
 # Misc. Kernels----------------------------------------------------------------
 

pyiid/scripts/benchmarking/au55_sim.py
@@ -20,6 +20,8 @@ atomsio = aseio.read(atoms_file)
 wrap_atoms(atomsio)
 
 qmax = 25
+# qmin = 2.5
+qmin = 0.
 qbin = .1
 n = len(atomsio)
 
@@ -27,13 +29,13 @@ qmax_bin = int(qmax / qbin)
 
 atoms = dc(atomsio)
 # view(atoms)
-pdf, fq = wrap_pdf(atoms, qmin=0, qbin=.1)
+pdf, fq = wrap_pdf(atoms, qmin=qmin, qbin=.1)
 atoms.positions *= 1.05
 # atoms.rattle(.05)
 # atoms[51].position += [1,0,0]
-rw, scale, apdf, afq = wrap_rw(atoms, pdf, qmin=0, qbin=.1)
+rw, scale, apdf, afq = wrap_rw(atoms, pdf, qmin=qmin, qbin=.1)
 
-calc = PDFCalc(gobs=pdf, qmin=0, conv=1, qbin=.1)
+calc = PDFCalc(gobs=pdf, qmin=qmin, conv=1, qbin=.1)
 atoms.set_calculator(calc)
 rwi = atoms.get_potential_energy()
 print(rwi)
@@ -63,8 +65,10 @@ for atoms in traj:
 '''
 
 view(traj)
-plt.plot(pdf, label='ideal')
-plt.plot(wrap_pdf(traj[0], qmin= 0)[0], label='start')
-plt.plot(wrap_pdf(traj[min_pe], qmin= 0)[0], label='best:' + str(min_pe))
+r = np.arange(0, 40, .01)
+plt.plot(r, pdf, label='ideal')
+plt.plot(r, wrap_pdf(traj[0], qmin= qmin)[0], label='start')
+plt.plot(r, wrap_pdf(traj[min_pe], qmin= qmin)[0], label='best:' + str(min_pe))
 plt.legend()
+plt.xlabel('r in A')
 plt.show()