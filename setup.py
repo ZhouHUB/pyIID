8075681156a89befc5f5b7969436f5e84967d3cc
CJ-Wright <cjwright4242@gmail.com>
2015-02-26 16:46:01 -0500

    FIX: build error due to package issues

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from distutils.core import setup
 setup(
     name='pyIID',
     version='1',
-    packages=['extra', 'pyiid', 'pyiid.sim', 'pyiid.calc', 'pyiid.test_kernels',
+    packages=['extra', 'pyiid', 'pyiid.sim', 'pyiid.calc', 'pyiid.tests',
               'pyiid.old_hmc', 'pyiid.scripts', 'pyiid.scripts.benchmarking',
-              'pyiid.test_wrappers'],
+              'pyiid.tests.test_kernels', 'pyiid.tests.test_wrappers'],
     url='',
     license='',
     author='christopher',
