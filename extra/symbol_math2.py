22ef0ab69f691c4b784e67a156e34b4be2343f7d
CJ-Wright <cjwright4242@gmail.com>
2015-03-16 08:30:24 -0400

    ENH: worked on scaled analytical gradient

--- extra/symbol_math2.py
+++ extra/symbol_math2.py
@@ -10,37 +10,17 @@ rmin, rmax = symbols('rmin rmax')
 gcalc = Function('gcalc')(x, r)
 Qmin, Qmax = symbols('Qmin Qmax')
 RW = symbols('RW')
-al = symbols('alpha')
+als = Function('alpha')(x,r)
 al = 1/summation(gcalc*gcalc, (r, rmin, rmax)) * summation(gcalc * gob(r), (r, rmin, rmax))
+# al = (gcalc*gcalc)**-1*(gcalc*gob)
 
-Rw = sqrt(summation((gob(r)-al*gcalc)**2, (r, rmin, rmax))/summation(gob(r)**2,
+Rw = sqrt(summation((gob(r)-als*gcalc)**2, (r, rmin, rmax))/summation(gob(r)**2,
           (r, rmin, rmax)))
-# Rw = sqrt(summation((gob(r)-al(x)*gcalc)**2, (r, rmin, rmax))/summation(gob(r)**2,
-#           (r, rmin, rmax)))
-# pprint(Rw)
-# print(latex(Rw))
+print(latex(Rw))
 sol = diff(Rw, x).simplify()
-# .subs(sqrt(summation((gob(r) - al * gcalc)**2, (r, rmin, rmax))/summation(gob(r)**2, (r, rmin, rmax))), RW)
-pprint(sol)
-dg = symbols('Delta_g')
-
-# print(latex(sol))
-pprint(sol.subs((al(x)*gcalc-(gob(r))), dg))
-pprint(dg)
-'''
-F = Function('F')(Q, x)
-gcalc = 2/pi*Integral(F*sin(Q*r), (Q,Qmin, Qmax))
-
-#print(latex(gcalc))
-#print(latex(diff(gcalc, x)))
-i, j = symbols(' i j')
-N = symbols('N')
-FQ = 1/(N*b**2)*summation(b(i,Q)*b(j, Q) * sin(Q*r(i,j, x))/r(i,j, x),(j, 0, j))
-#print(latex(FQ))
-#print(latex(diff(FQ, x).simplify()))
-pprint(diff(FQ, x).simplify())
-
-#print(latex(rij))
-#print(latex(diff(rij, sax)))
-
-pprint(FQ)'''
+ssol = sol.subs(sqrt(summation((gob(r) - als * gcalc)**2, (r, rmin, rmax))/summation(gob(r)**2, (r, rmin, rmax))), RW)
+# ssol = ssol.subs(al, als)
+# pprint(ssol)
+print(latex(ssol))
+print '\n\n\n'
+print(latex(diff(al, x).subs(al, als)))
