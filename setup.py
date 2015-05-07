9759e281272004b1d976609bec8f804bf5626cad
CJ-Wright <cjwright4242@gmail.com>
2015-05-07 12:55:57 -0400

    MTN: Fix setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='',
     packages=['pyiid', 'pyiid.sim', 'pyiid.calc', 'pyiid.tests',
               'pyiid.tests.test_kernels', 'pyiid.tests.test_wrappers',
-              'pyiid.kernels', 'pyiid.scripts', 'pyiid.scripts.benchmarking',
+              'pyiid.kernels',
               'pyiid.testing', 'pyiid.wrappers', 'pyiid.wrappers.mpi',
               ],
     url='',
