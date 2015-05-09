b412884867d3b9789874d835eaa11706dd592ddc
Christopher J. Wright <cjwright4242@gmail.com>
2015-05-09 18:13:11 -0400

    TST: known gpu fail

--- pyiid/tests/test_cpu/test_scatter_basic.py
+++ pyiid/tests/test_cpu/test_scatter_basic.py
@@ -2,9 +2,28 @@ __author__ = 'christopher'
 import numpy as np
 from pyiid.wrappers.scatter import Scatter
 from pyiid.tests import setup_atoms, generate_experiment
+from pyiid.testing.decorators import known_fail_if
 
 
 def test_scatter():
+    for i in range(4):
+        if i == 0:
+            exp = None
+        else:
+            exp = generate_experiment()
+        scat = Scatter(exp_dict=exp)
+        # Test a set of different sized ensembles
+        for n in np.logspace(1, 4, 4):
+            atoms = setup_atoms(int(n))
+            fq = scat.fq(atoms)
+            assert fq is not None
+            assert fq != np.zeros(fq.shape)
+            pdf = scat.pdf(atoms)
+            assert pdf is not None
+            assert pdf != np.zeros(pdf.shape)
+
+@known_fail_if(True)
+def test_gpu_scatter_fail():
     for i in range(4):
         if i == 0:
             exp = None
