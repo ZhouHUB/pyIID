31470d63fcd635122f07b5d9b468ebe32e8245be
Christopher J. Wright <cjwright4242@gmail.com>
2015-05-10 13:00:18 -0400

    TST: defaults vs random parameters

--- pyiid/tests/test_cpu/test_scatter_basic.py
+++ pyiid/tests/test_cpu/test_scatter_basic.py
@@ -5,12 +5,8 @@ from pyiid.tests import setup_atoms, generate_experiment
 from pyiid.testing.decorators import known_fail_if
 
 
-def test_scatter_fq():
-    for i in range(4):
-        if i == 0:
-            exp = None
-        else:
-            exp = generate_experiment()
+def test_scatter_fq_defaults():
+        exp = None
         scat = Scatter(exp_dict=exp)
         # Test a set of different sized ensembles
         for n in np.logspace(1, 3, 3):
@@ -21,12 +17,22 @@ def test_scatter_fq():
             # Check that all the values are not zero
             assert np.any(fq)
 
-def test_scatter_sq():
-    for i in range(4):
-        if i == 0:
-            exp = None
-        else:
-            exp = generate_experiment()
+def test_scatter_fq():
+    for i in range(3):
+        exp = generate_experiment()
+        scat = Scatter(exp_dict=exp)
+        # Test a set of different sized ensembles
+        for n in np.logspace(1, 3, 3):
+            atoms = setup_atoms(int(n))
+            fq = scat.get_fq(atoms)
+            # Check that Scatter gave back something
+            assert fq is not None
+            # Check that all the values are not zero
+            assert np.any(fq)
+
+
+def test_scatter_sq_defaults():
+        exp = None
         scat = Scatter(exp_dict=exp)
         # Test a set of different sized ensembles
         for n in np.logspace(1, 3, 3):
@@ -37,12 +43,23 @@ def test_scatter_sq():
             # Check that all the values are not zero
             assert np.any(sq)
 
-def test_scatter_iq():
-    for i in range(4):
-        if i == 0:
-            exp = None
-        else:
-            exp = generate_experiment()
+
+def test_scatter_sq():
+    for i in range(3):
+        exp = generate_experiment()
+        scat = Scatter(exp_dict=exp)
+        # Test a set of different sized ensembles
+        for n in np.logspace(1, 3, 3):
+            atoms = setup_atoms(int(n))
+            sq = scat.get_sq(atoms)
+            # Check that Scatter gave back something
+            assert sq is not None
+            # Check that all the values are not zero
+            assert np.any(sq)
+
+
+def test_scatter_iq_defaults():
+        exp = None
         scat = Scatter(exp_dict=exp)
         # Test a set of different sized ensembles
         for n in np.logspace(1, 3, 3):
@@ -54,12 +71,22 @@ def test_scatter_iq():
             assert np.any(iq)
 
 
-def test_scatter_pdf():
-    for i in range(4):
-        if i == 0:
-            exp = None
-        else:
-            exp = generate_experiment()
+def test_scatter_iq():
+    for i in range(3):
+        exp = generate_experiment()
+        scat = Scatter(exp_dict=exp)
+        # Test a set of different sized ensembles
+        for n in np.logspace(1, 3, 3):
+            atoms = setup_atoms(int(n))
+            iq = scat.get_iq(atoms)
+            # Check that Scatter gave back something
+            assert iq is not None
+            # Check that all the values are not zero
+            assert np.any(iq)
+
+
+def test_scatter_pdf_defaults():
+        exp = None
         scat = Scatter(exp_dict=exp)
         # Test a set of different sized ensembles
         for n in np.logspace(1, 3, 3):
@@ -71,12 +98,22 @@ def test_scatter_pdf():
             assert np.any(pdf)
 
 
-def test_scatter_grad_fq():
-    for i in range(4):
-        if i == 0:
-            exp = None
-        else:
-            exp = generate_experiment()
+def test_scatter_pdf():
+    for i in range(3):
+        exp = generate_experiment()
+        scat = Scatter(exp_dict=exp)
+        # Test a set of different sized ensembles
+        for n in np.logspace(1, 3, 3):
+            atoms = setup_atoms(int(n))
+            pdf = scat.get_pdf(atoms)
+            # Check that Scatter gave back something
+            assert pdf is not None
+            # Check that all the values are not zero
+            assert np.any(pdf)
+
+
+def test_scatter_grad_fq_defaults():
+        exp = None
         scat = Scatter(exp_dict=exp)
         # Test a set of different sized ensembles
         for n in np.logspace(1, 3, 3):
@@ -88,12 +125,22 @@ def test_scatter_grad_fq():
             assert np.any(grad_fq)
 
 
-def test_scatter_grad_pdf():
-    for i in range(4):
-        if i == 0:
-            exp = None
-        else:
-            exp = generate_experiment()
+def test_scatter_grad_fq():
+    for i in range(3):
+        exp = generate_experiment()
+        scat = Scatter(exp_dict=exp)
+        # Test a set of different sized ensembles
+        for n in np.logspace(1, 3, 3):
+            atoms = setup_atoms(int(n))
+            grad_fq = scat.get_grad_fq(atoms)
+            # Check that Scatter gave back something
+            assert grad_fq is not None
+            # Check that all the values are not zero
+            assert np.any(grad_fq)
+
+
+def test_scatter_grad_pdf_defaults():
+        exp = None
         scat = Scatter(exp_dict=exp)
         # Test a set of different sized ensembles
         for n in np.logspace(1, 3, 3):
@@ -104,6 +151,19 @@ def test_scatter_grad_pdf():
             # Check that all the values are not zero
             assert np.any(grad_pdf)
 
+
+def test_scatter_grad_pdf():
+    for i in range(3):
+        exp = generate_experiment()
+        scat = Scatter(exp_dict=exp)
+        # Test a set of different sized ensembles
+        for n in np.logspace(1, 3, 3):
+            atoms = setup_atoms(int(n))
+            grad_pdf = scat.get_grad_pdf(atoms)
+            # Check that Scatter gave back something
+            assert grad_pdf is not None
+            # Check that all the values are not zero
+            assert np.any(grad_pdf)
 
 @known_fail_if(True)
 def test_gpu_scatter_fail():
