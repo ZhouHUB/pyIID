b033891f2b90f94d71f5ba290ddecf83e1cda81c
Christopher J. Wright <cjwright4242@gmail.com>
2015-05-10 12:37:31 -0400

    TST: add sq, iq

--- pyiid/tests/test_cpu/test_scatter_basic.py
+++ pyiid/tests/test_cpu/test_scatter_basic.py
@@ -16,11 +16,42 @@ def test_scatter_fq():
         for n in np.logspace(1, 3, 3):
             atoms = setup_atoms(int(n))
             fq = scat.get_fq(atoms)
-            print fq
             # Check that Scatter gave back something
             assert fq is not None
             # Check that all the values are not zero
             assert np.any(fq)
+
+def test_scatter_fq():
+    for i in range(4):
+        if i == 0:
+            exp = None
+        else:
+            exp = generate_experiment()
+        scat = Scatter(exp_dict=exp)
+        # Test a set of different sized ensembles
+        for n in np.logspace(1, 3, 3):
+            atoms = setup_atoms(int(n))
+            sq = scat.get_sq(atoms)
+            # Check that Scatter gave back something
+            assert sq is not None
+            # Check that all the values are not zero
+            assert np.any(sq)
+
+def test_scatter_fq():
+    for i in range(4):
+        if i == 0:
+            exp = None
+        else:
+            exp = generate_experiment()
+        scat = Scatter(exp_dict=exp)
+        # Test a set of different sized ensembles
+        for n in np.logspace(1, 3, 3):
+            atoms = setup_atoms(int(n))
+            iq = scat.get_iq(atoms)
+            # Check that Scatter gave back something
+            assert iq is not None
+            # Check that all the values are not zero
+            assert np.any(iq)
 
 
 def test_scatter_pdf():
