BUG: scatter broadcast error

pyiid/wrappers/scatter.py
@@ -114,7 +114,7 @@ class Scatter(object):
     def get_sq(self, atoms):
         fq = self.fq(atoms)
         scatter_vector = np.arange(0, self.exp['qmax'], self.exp['qbin'])
-        return fq / scatter_vector + np.ones(len(scatter_vector))
+        return (fq / scatter_vector) + np.ones(scatter_vector.shape)
 
     def get_iq(self, atoms):
         return self.get_sq(atoms) * np.average(atoms.get_array('scatter')) ** 2