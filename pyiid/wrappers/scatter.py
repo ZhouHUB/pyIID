9a24f141cad12da6e447fa0838d594d55c177ec8
CJ-Wright <cjwright4242@gmail.com>
2015-06-02 19:59:44 -0400

    ENH: scatter now checks to make certain that the atoms scatter factor was generated with the correct Q vector

--- pyiid/wrappers/scatter.py
+++ pyiid/wrappers/scatter.py
@@ -3,6 +3,7 @@ from numba import cuda
 import numpy as np
 import math
 import sys
+
 sys.path.extend(['/mnt/work-data/dev/pyIID'])
 
 from pyiid.kernels.master_kernel import get_pdf_at_qmin, grad_pdf, \
@@ -37,18 +38,9 @@ class Scatter(object):
     def __init__(self, exp_dict=None):
         # Currently supported processor architectures
         self.avail_pro = ['MPI-GPU', 'Multi-GPU', 'CPU']
-
-        # Should be read in from the gr file, but if not here are some defaults
-        if exp_dict is None or bool(exp_dict) is False:
-            exp_dict = {'qmin': 0.0, 'qmax': 25., 'qbin': .1, 'rmin': 0.0,
-                        'rmax': 40.0, 'rstep': .01}
-        # Check keys
-
-
-        self.exp = exp_dict
-        # Technically we should use this for qbin:
-        self.exp['qbin'] = np.pi / (self.exp['rmax'] + 6 * 2 * np.pi /
-                                    self.exp['qmax'])
+        self.exp = None
+        self.Q = None
+        self.update_experiment(exp_dict)
 
         # Just in case something blows up down the line
         self.fq = cpu_wrap_fq
@@ -59,8 +51,24 @@ class Scatter(object):
         self.set_processor()
 
     def check_scatter(self, atoms):
-        if 'scatter' not in atoms.arrays.keys():
+        if 'scatter' not in atoms.arrays.keys() or np.any(
+                        atoms.info['Q'] != self.Q):
             wrap_atoms(atoms, self.exp)
+
+    def update_experiment(self, exp_dict):
+        # Should be read in from the gr file, but if not here are some defaults
+        if exp_dict is None or bool(exp_dict) is False:
+            exp_dict = {'qmin': 0.0, 'qmax': 25., 'qbin': .1, 'rmin': 0.0,
+                        'rmax': 40.0, 'rstep': .01}
+        # TODO: Check keys to make certain everything is there
+
+        self.exp = exp_dict
+        # Technically we should use this for qbin:
+        self.exp['qbin'] = np.pi / (self.exp['rmax'] + 6 * 2 * np.pi /
+                                    self.exp['qmax'])
+
+        self.Q = np.arange(exp_dict['qmin'], exp_dict['qmax'],
+                           exp_dict['qbin'])
 
     def set_processor(self, processor=None):
         """
@@ -83,7 +91,8 @@ class Scatter(object):
                     break
 
         elif processor == self.avail_pro[0] and check_mpi() is True:
-            from pyiid.wrappers.mpi_gpu_wrap import wrap_fq as multi_node_gpu_wrap_fq
+            from pyiid.wrappers.mpi_gpu_wrap import \
+                wrap_fq as multi_node_gpu_wrap_fq
             from pyiid.wrappers.mpi_gpu_wrap import \
                 wrap_fq_grad as multi_node_gpu_wrap_fq_grad
 
@@ -93,10 +102,10 @@ class Scatter(object):
             return True
 
         elif processor == self.avail_pro[1] and check_multi_gpu() is True:
-            from pyiid.wrappers.multi_gpu_wrap import wrap_fq as node_0_gpu_wrap_fq
+            from pyiid.wrappers.multi_gpu_wrap import \
+                wrap_fq as node_0_gpu_wrap_fq
             from pyiid.wrappers.multi_gpu_wrap import \
                 wrap_fq_grad as node_0_gpu_wrap_fq_grad
-
 
             from pyiid.wrappers.flat_gpu_wrap import wrap_fq as flat_fq
             from pyiid.wrappers.flat_gpu_wrap import \
@@ -181,7 +190,7 @@ def wrap_atoms(atoms, exp_dict=None):
 
     if exp_dict is None:
         exp_dict = {'qmin': 0.0, 'qmax': 25., 'qbin': .1, 'rmin': 0.0,
-                            'rmax': 40.0, 'rstep': .01}
+                    'rmax': 40.0, 'rstep': .01}
     qbin = np.pi / (exp_dict['rmax'] + 6 * 2 * np.pi / exp_dict['qmax'])
     n = len(atoms)
     qmax_bin = int(math.ceil(exp_dict['qmax'] / qbin))
@@ -190,7 +199,7 @@ def wrap_atoms(atoms, exp_dict=None):
     scatter_array = np.zeros((n, qmax_bin), dtype=np.float32)
     get_scatter_array(scatter_array, e_num, qbin)
     atoms.set_array('scatter', scatter_array)
-    # atoms.set_array('Q', np.arange(exp_dict['qmin'], exp_dict['qmax'], qbin))
+    atoms.info['Q']= np.arange(exp_dict['qmin'], exp_dict['qmax'], qbin)
 
 
 if __name__ == '__main__':
@@ -205,8 +214,8 @@ if __name__ == '__main__':
                 'qbin': np.pi / (45. + 6 * 2 * np.pi / 25), 'rmin': 0.0,
                 'rmax': 40.0, 'rstep': .01}
     # wrap_atoms(atoms, exp_dict)
-    scat = Scatter(exp_dict)
-
+    # scat = Scatter(exp_dict)
+    scat = Scatter()
     # fq = scat.get_fq(atoms)
     gfq = scat.get_grad_fq(atoms)
     # pdf = scat.get_pdf(atoms)
