3488bd1898082269e7d615afaee0bc485217a814
CJ-Wright <cjwright4242@gmail.com>
2015-07-20 12:15:54 -0400

    TST: fix dynamics tests to be shorter/travis ready

--- pyiid/tests/test_master/test_sim/test_dynamics.py
+++ pyiid/tests/test_master/test_sim/test_dynamics.py
@@ -7,7 +7,6 @@ from ase.atoms import Atoms
 from pyiid.wrappers.elasticscatter import ElasticScatter
 from pyiid.calc.pdfcalc import PDFCalc
 
-
 def test_dynamics():
 
     ideal_atoms = Atoms('Au4', [[0, 0, 0], [3, 0, 0], [0, 3, 0], [3, 3, 0]])
@@ -17,43 +16,41 @@ def test_dynamics():
 
     ideal_atoms.positions *= 1.02
 
-    calc = PDFCalc(obs_data=gobs, scatter=s, conv=300, potential='rw')
+    calc = PDFCalc(obs_data=gobs, scatter=s, conv=1, potential='rw')
     ideal_atoms.set_calculator(calc)
-    print ideal_atoms.get_potential_energy() / 300
 
-    e = .05
+    e = .5
 
-    traj = classical_dynamics(ideal_atoms, e, 200)
+    traj = classical_dynamics(ideal_atoms, e, 10)
 
     pe_list = []
     for atoms in traj:
         pe_list.append(atoms.get_potential_energy())
-    min_pe = np.argmin(pe_list)
-    assert min_pe / 300. < .1
-
+    min_pe = np.min(pe_list)
+    assert min_pe < .1
 
 def test_reverse_dynamics():
 
     ideal_atoms = Atoms('Au4', [[0, 0, 0], [3, 0, 0], [0, 3, 0], [3, 3, 0]])
     ideal_atoms.set_velocities(np.zeros((len(ideal_atoms), 3)))
     s = ElasticScatter()
+    s.set_processor('CPU', 'flat')
     gobs = s.get_pdf(ideal_atoms)
 
     ideal_atoms.positions *= 1.02
 
-    calc = PDFCalc(obs_data=gobs, scatter=s, conv=300, potential='rw')
+    calc = PDFCalc(obs_data=gobs, scatter=s, conv=1, potential='rw')
     ideal_atoms.set_calculator(calc)
-    print ideal_atoms.get_potential_energy() / 300
 
-    e = -.05
+    e = -.5
 
-    traj = classical_dynamics(ideal_atoms, e, 200)
+    traj = classical_dynamics(ideal_atoms, e, 10)
 
     pe_list = []
     for atoms in traj:
         pe_list.append(atoms.get_potential_energy())
-    min_pe = np.argmin(pe_list)
-    assert min_pe / 300. < .1
+    min_pe = np.min(pe_list)
+    assert min_pe < .1
 
 if __name__ == '__main__':
     import nose
