89880b919a0b0d0e08dc90e15e6e9056dd5b906b
Christopher J. Wright <cjwright4242@gmail.com>
2015-08-23 19:14:13 -0400

    BLD: new setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
-import setuptools
 from distutils.core import setup
 
 setup(
     name='pyIID',
     version='',
     packages=['extra', 'pyiid', 'pyiid.sim', 'pyiid.calc', 'pyiid.tests',
-              'pyiid.tests.test_master', 'pyiid.kernels', 'pyiid.testing',
-              'pyiid.experiments', 'pyiid.experiments.mpi'],
+              'pyiid.tests.test_master', 'pyiid.tests.test_master.test_sim',
+              'pyiid.tests.test_master.test_calc', 'pyiid.kernels',
+              'pyiid.testing', 'pyiid.experiments', 'pyiid.experiments.rixs',
+              'pyiid.experiments.saxs', 'pyiid.experiments.exafs',
+              'pyiid.experiments.elasticscatter',
+              'pyiid.experiments.elasticscatter.mpi',
+              'pyiid.experiments.elasticscatter.cpu_wrappers',
+              'pyiid.experiments.elasticscatter.gpu_wrappers', 'benchmarks',
+              'benchmarks.speed'],
     url='',
     license='',
-    author='Christopher J. Wright',
-    author_email='wright1@email.sc.edu',
-    description='',
-    requires=['numba', 'numpy', 'ase', 'nose', 'mpi4py', 'simdb', 'matplotlib',
-              'matplotlib', 'psutil', 'six']
+    author='christopher',
+    author_email='',
+    description=''
 )
